a7444ab5da8233913aa2117dff2595d662dd77f7
alunoEtec <aula@etec.com>
2025-02-11 20:00:29 -0300

    Criação do arquivo index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projeto PWII b</title>
+</head>
+<body>
+  <?php 
+echo "<h1> titulo do projeto PWII B</h1>";
+  ?> 
+</body>
+</html>
