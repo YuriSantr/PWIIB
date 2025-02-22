032c887b0ddb22f2c259c0c5b29fca918799f79e
alunoEtec <aula@etec.com>
2025-02-21 22:41:09 -0300

    aula 21/02

--- index.php
+++ index.php
@@ -6,217 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>projeto PWII b</title>
    <link rel="stylesheet" href="bootstrap.min.css"></a>
-   <Style>
-   .row{
-    border: 2px solid red;
-   }
-   .col{
-     border: 2px solid pink ;
-     padding: 50px 0;
-     text-align: center;
-   }
-   </style>
 </head>
 <body>
 
-  <div class="row">
-   <div class="col-3">
-   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Login</button>
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Cadastro</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Login:</label>
-            <input type="text" class="form-control" id="recipient-name">
-          </div>
-          <div class="mb-3">
-            <label for="message-text" class="col-form-label">Senha:</label>
-            <textarea class="form-control" id="message-text"></textarea>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <button type="button" class="btn btn-primary">Entrar</button>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-   
-    <div class="col-9">
-    <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand">pesquisar</a>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">procurar</button>
-    </form>
-  </div>
-</nav>
-    </div>
-  </div>
-
-
-
-
-
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+  <img src="./img/sk.jpg" alt="Bootstrap" width="30" height="24">
+    <a class="navbar-brand" img="#" href=" #">Projeto PW 2 Turma B</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
+        <li class="nav-item dropdown">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            PHP
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="variavel.php">variavel</a></li>
+            <li><a class="dropdown-item" href="if.php">if</a></li>
+            <li><a class="dropdown-item" href="while.php">while</a></li>
+            <li><a class="dropdown-item" href="for.php">for</a></li>
+            <li><a class="dropdown-item" href="switch.php">switch</a></li>
+            <li><a class="dropdown-item" href="array/vetor.php">array/vetor</a></li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
-
-<h1>Sistema de layout
-
-</h1>
-
-
-
-<br>
-<div class="card">
-  <div class="card-header">
-    <h2>cadastro de ususarios</h2>
-  </div>
-  <div class="card-body">
-  <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-  </div>
-</div>
-<br>
-
-  <a href= "contato.php"> Página de contato </a>
-  <button type="button" class="btn btn-primary">Primary</button>
-  <?php 
-echo "<h1> titulo do projeto PWII B</h1>";
-  ?> 
-
- <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  confirmação
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">tem certeza que deseja sair?</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        voce esta prestes a sair do sistema, deseja realmente sair?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">nao sair</button>
-        <button type="button" class="btn btn-primary">sim</button>
-      </div>
-    </div>
-  </div>
-</div>
-<br>
-
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    menu de opções
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="https://globo.com">acessar globo.com</a></li>
-    <li><a class="dropdown-item" href="https://oul.com.br">acessar oul.com</a></li>
-    <li><a class="dropdown-item" href="usuario.php">cadastrar usuarios</a></li>
-  </ul>
-</div>
-<br>
-
-<ul class="list-group">
-  <li class="list-group-item">pao</li>
-  <li class="list-group-item">arroz</li>
-  <li class="list-group-item">picanha</li>
-  <li class="list-group-item">rtx5090</li>
-  <li class="list-group-item">farinha</li>
-</ul>
-<br> <br>
-<table class="table table-hover table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-  <script src="bootstrap.bundle.min.js"></script>
-</body>
-</html>
